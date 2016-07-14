4d4699c8f046d4908b5283a26b3a9b4ff1785a5e
Alexey Kuznetsov <axet@me.com>
2016-07-14 10:48:26 +0300

    add conn6

--- lpd.go
+++ lpd.go
@@ -23,63 +23,60 @@ var lpd *LPDServer
 // TODO http://bittorrent.org/beps/bep_0026.html
 
 const (
-	bep14_host     = "239.192.152.143:6771"
+	bep14_host4    = "239.192.152.143:6771"
+	bep14_host6    = "[ff15::efc0:988f]:6771"
 	bep14_announce = "BT-SEARCH * HTTP/1.1\r\n" +
 		"Host: %s\r\n" +
 		"Port: %s\r\n" +
-		"Infohash: %s\r\n\r\n"
-	bep14_long_timeout  = 5 * time.Minute
-	bep14_short_timeout = 1 * time.Minute
-	bep14_max           = 1 // maximum hashes per request, 0 unlimited
+		"%s" +
+		"\r\n" +
+		"\r\n"
+	bep14_announce_infohash = "Infohash: %s\r\n"
+	bep14_long_timeout      = 5 * time.Minute
+	bep14_short_timeout     = 1 * time.Minute
+	bep14_max               = 1 // maximum hashes per request, 0 unlimited
 )
 
-type LPDServer struct {
-	stop  missinggo.Event
-	force missinggo.Event
-
-	addr  *net.UDPAddr
-	conn  *net.UDPConn
-	peers map[int64]string // active local peers
-
-	queue []*torrent.Torrent
-	next  *torrent.Torrent
+type LPDConn struct {
+	network string // "udp4" or "udp6"
+	addr    *net.UDPAddr
+	conn    *net.UDPConn
+	host    string // bep14_host4 or bep14_host6
 }
 
-func lpdStart() {
-	lpd = &LPDServer{}
+func lpdConnNew(network string, host string) *LPDConn {
+	m := &LPDConn{}
 
-	lpd.peers = make(map[int64]string)
+	m.network = network
+	m.host = host
 
-	lpd.addr, err = net.ResolveUDPAddr("udp4", bep14_host)
+	var err error
+
+	m.addr, err = net.ResolveUDPAddr(m.network, m.host)
 	if err != nil {
-		log.Println("LPT unable to start", err)
-		return
+		log.Println("LPD unable to start", err)
+		return nil
+	}
+	m.conn, err = net.ListenMulticastUDP(m.network, nil, m.addr)
+	if err != nil {
+		log.Println("LPD unable to start", err)
+		return nil
 	}
 
-	lpd.conn, err = net.ListenMulticastUDP("udp4", nil, lpd.addr)
-	if err != nil {
-		log.Println("LPT unable to start", err)
-		return
-	}
-
-	go lpd.receiver()
-	go lpd.announcer()
-
-	return
+	return m
 }
 
-func (m *LPDServer) receiver() {
+func (m *LPDConn) receiver() {
 	for {
 		mu.Lock()
 		if lpd == nil {
 			mu.Unlock()
 			return
 		}
-		conn := lpd.conn
 		mu.Unlock()
 
 		buf := make([]byte, 2000)
-		_, from, err := conn.ReadFromUDP(buf)
+		_, from, err := m.conn.ReadFromUDP(buf)
 		if err != nil {
 			log.Println("Local Announce read error: ", err)
 			continue
@@ -102,38 +99,170 @@ func (m *LPDServer) receiver() {
 			continue
 		}
 
-		var ihs []string
-		for _, v := range strings.Split(ih, " ") {
-			v = strings.TrimSpace(v)
-			for i := 0; i < len(v); i += 40 {
-				h := v[i : i+40]
-				ihs = append(ihs, h)
-			}
-		}
-
 		port := req.Header.Get("Port")
 		if port == "" {
 			log.Println("No port")
 			continue
 		}
 
-		addr, err := net.ResolveTCPAddr("tcp4", net.JoinHostPort(from.IP.String(), port))
+		addr, err := net.ResolveTCPAddr("tcp", net.JoinHostPort(from.IP.String(), port))
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
 		mu.Lock()
-		m.peer(addr.String())
-		m.refresh()
-		for _, h := range ihs {
-			hash := metainfo.NewHashFromHex(h)
-			if t, ok := client.Torrent(hash); ok {
-				lpdPeer(t, addr.String())
-			}
+		lpd.peer(addr.String())
+		lpd.refresh()
+		//log.Println("LPD", addr.String(), ih)
+		hash := metainfo.NewHashFromHex(ih)
+		if t, ok := client.Torrent(hash); ok {
+			lpdPeer(t, addr.String())
 		}
 		mu.Unlock()
 	}
+}
+
+func (m *LPDConn) announcer() {
+	var refresh time.Duration = 0
+	var next *torrent.Torrent
+	var queue []*torrent.Torrent
+
+	for {
+		mu.Lock()
+		lpd.force.Clear()
+		mu.Unlock()
+
+		//log.Println("LPD", refresh)
+
+		select {
+		case <-lpd.stop.LockedChan(&mu):
+			return
+		case <-lpd.force.LockedChan(&mu):
+		case <-time.After(refresh):
+		}
+
+		mu.Lock()
+		// add missing torrent to send queue
+		for t := range active {
+			if _, ok := lpdContains(queue, t); !ok {
+				queue = append(queue, t)
+			}
+		}
+
+		if next == nil {
+			if len(queue) > 0 {
+				next = queue[0]
+			}
+		}
+
+		// remove stopped torrent from queue
+		var remove []*torrent.Torrent
+		for _, t := range queue {
+			if _, ok := active[t]; !ok {
+				remove = append(remove, t)
+			}
+		}
+		for _, t := range remove {
+			if i, ok := lpdContains(queue, t); ok {
+				if next == t { // update next to next+1
+					n := i + 1
+					if n >= len(queue) {
+						next = nil
+					} else {
+						next = queue[n]
+					}
+				}
+				queue = append(queue[:i], queue[i+1:]...)
+			}
+		}
+		lpd.refresh()
+
+		var ihs string
+		var old []byte
+
+		_, port, err := net.SplitHostPort(clientAddr)
+		if err != nil {
+			mu.Unlock()
+			log.Println("Announce error", err)
+			continue
+		}
+		count := 0
+		for next != nil {
+			ihs += fmt.Sprintf(bep14_announce_infohash, strings.ToUpper(next.InfoHash().HexString()))
+			req := fmt.Sprintf(bep14_announce, m.host, port, ihs)
+			buf := []byte(req)
+			if len(buf) >= 1400 {
+				break
+			}
+			old = buf
+			if i, ok := lpdContains(queue, next); ok {
+				i++
+				if i >= len(queue) {
+					next = nil
+				} else {
+					next = queue[i]
+				}
+			}
+			count++
+			if bep14_max > 0 && count >= bep14_max {
+				break
+			}
+		}
+		mu.Unlock()
+
+		if len(old) > 0 {
+			//log.Println("LPD", string(old), len(old))
+			_, err = m.conn.WriteToUDP(old, m.addr)
+			if err != nil {
+				log.Println("Announce error", err)
+			}
+		}
+
+		// sholud we wait for 5 min or 1 min, if we still announcing
+		refresh = bep14_short_timeout
+		if next == nil { // restart queue
+			refresh = bep14_long_timeout
+		}
+	}
+}
+
+func (m *LPDConn) Close() {
+	if m.conn != nil {
+		m.conn.Close()
+		m.conn = nil
+	}
+}
+
+type LPDServer struct {
+	stop  missinggo.Event
+	force missinggo.Event
+
+	conn4 *LPDConn
+	conn6 *LPDConn
+
+	peers map[int64]string // active local peers
+}
+
+func lpdStart() {
+	lpd = &LPDServer{}
+
+	lpd.peers = make(map[int64]string)
+
+	lpd.conn4 = lpdConnNew("udp4", bep14_host4)
+	if lpd.conn4 != nil {
+		go lpd.conn4.receiver()
+		go lpd.conn4.announcer()
+
+	}
+
+	//lpd.conn6 = lpdConnNew("udp6", bep14_host6)
+	if lpd.conn6 != nil {
+		go lpd.conn6.receiver()
+		go lpd.conn6.announcer()
+	}
+
+	return
 }
 
 func (m *LPDServer) refresh() {
@@ -162,113 +291,13 @@ func (m *LPDServer) peer(peer string) {
 	m.peers[now] = peer
 }
 
-func (m *LPDServer) contains(e *torrent.Torrent) (int, bool) {
-	for i, t := range m.queue {
+func lpdContains(queue []*torrent.Torrent, e *torrent.Torrent) (int, bool) {
+	for i, t := range queue {
 		if t == e {
 			return i, true
 		}
 	}
 	return -1, false
-}
-
-func (m *LPDServer) announcer() {
-	var refresh time.Duration = 0
-
-	for {
-		mu.Lock()
-		m.force.Clear()
-		mu.Unlock()
-
-		select {
-		case <-m.stop.LockedChan(&mu):
-			return
-		case <-m.force.LockedChan(&mu):
-		case <-time.After(refresh):
-		}
-
-		m.refresh()
-
-		mu.Lock()
-		// add missing torrent to send queue
-		for t := range active {
-			if _, ok := m.contains(t); !ok {
-				m.queue = append(m.queue, t)
-			}
-		}
-
-		if m.next == nil {
-			if len(m.queue) > 0 {
-				m.next = m.queue[0]
-			}
-		}
-
-		// remove stopped torrent from queue
-		var remove []*torrent.Torrent
-		for _, t := range m.queue {
-			if _, ok := active[t]; !ok {
-				remove = append(remove, t)
-			}
-		}
-		for _, t := range remove {
-			if i, ok := m.contains(t); ok {
-				if m.next == t { // update next to next+1
-					n := i + 1
-					if n >= len(m.queue) {
-						m.next = nil
-					} else {
-						m.next = m.queue[n]
-					}
-				}
-				m.queue = append(m.queue[:i], m.queue[i+1:]...)
-			}
-		}
-		mu.Unlock()
-
-		var ihs string
-		var old []byte
-
-		mu.Lock()
-		_, port, err := net.SplitHostPort(clientAddr)
-		if err != nil {
-			mu.Unlock()
-			log.Println("Announce error", err)
-			continue
-		}
-		count := 0
-		for m.next != nil {
-			ihs += m.next.InfoHash().HexString()
-			req := fmt.Sprintf(bep14_announce, bep14_host, port, strings.ToUpper(ihs))
-			buf := []byte(req)
-			if len(buf) >= 1400 {
-				break
-			}
-			old = buf
-			if i, ok := m.contains(m.next); ok {
-				i++
-				if i >= len(m.queue) {
-					m.next = nil
-				} else {
-					m.next = m.queue[i]
-				}
-			}
-			count++
-			if bep14_max != 0 && count >= bep14_max {
-				break
-			}
-		}
-		mu.Unlock()
-
-		_, err = m.conn.WriteToUDP(old, m.addr)
-		if err != nil {
-			log.Println("Announce error", err)
-		}
-
-		// sholud we wait for 5 min or 1 min, if we still announcing
-		refresh = bep14_short_timeout
-		if m.next == nil { // restart queue
-			refresh = bep14_long_timeout
-		}
-	}
 }
 
 func lpdForce() {
@@ -277,7 +306,8 @@ func lpdForce() {
 
 func lpdStop() {
 	if lpd != nil {
-		lpd.conn.Close()
+		lpd.conn4.Close()
+		lpd.conn6.Close()
 		lpd.stop.Set()
 		lpd = nil
 	}
